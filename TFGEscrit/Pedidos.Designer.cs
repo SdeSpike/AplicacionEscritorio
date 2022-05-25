8247ae6cd4c9901bb66277e0571151adb8584b90
SdeSpike <101330778+SdeSpike@users.noreply.github.com>
2022-05-25 12:54:11 +0200

    Cambios
    C

--- TFGEscrit/Pedidos.Designer.cs
+++ TFGEscrit/Pedidos.Designer.cs
@@ -32,7 +32,11 @@ namespace TFGEscrit
             this.cmbCategorias = new System.Windows.Forms.ComboBox();
             this.lstProductos = new System.Windows.Forms.ListBox();
             this.grdMateriaPrima = new System.Windows.Forms.DataGridView();
+            this.grdPedidos = new System.Windows.Forms.DataGridView();
+            this.btnPedido = new System.Windows.Forms.Button();
+            this.btnHacerPedido = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.grdMateriaPrima)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPedidos)).BeginInit();
             this.SuspendLayout();
             // 
             // cmbCategorias
@@ -55,17 +59,56 @@ namespace TFGEscrit
             // 
             // grdMateriaPrima
             // 
+            this.grdMateriaPrima.AllowUserToAddRows = false;
+            this.grdMateriaPrima.AllowUserToDeleteRows = false;
             this.grdMateriaPrima.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.grdMateriaPrima.Location = new System.Drawing.Point(581, 87);
             this.grdMateriaPrima.Name = "grdMateriaPrima";
+            this.grdMateriaPrima.ReadOnly = true;
+            this.grdMateriaPrima.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.grdMateriaPrima.Size = new System.Drawing.Size(805, 290);
             this.grdMateriaPrima.TabIndex = 4;
+            // 
+            // grdPedidos
+            // 
+            this.grdPedidos.AllowUserToAddRows = false;
+            this.grdPedidos.AllowUserToDeleteRows = false;
+            this.grdPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPedidos.Location = new System.Drawing.Point(59, 413);
+            this.grdPedidos.Name = "grdPedidos";
+            this.grdPedidos.ReadOnly = true;
+            this.grdPedidos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdPedidos.Size = new System.Drawing.Size(817, 215);
+            this.grdPedidos.TabIndex = 5;
+            // 
+            // btnPedido
+            // 
+            this.btnPedido.Location = new System.Drawing.Point(1242, 397);
+            this.btnPedido.Name = "btnPedido";
+            this.btnPedido.Size = new System.Drawing.Size(130, 23);
+            this.btnPedido.TabIndex = 6;
+            this.btnPedido.Text = "Agregar al pedido";
+            this.btnPedido.UseVisualStyleBackColor = true;
+            this.btnPedido.Click += new System.EventHandler(this.btnPedido_Click);
+            // 
+            // btnHacerPedido
+            // 
+            this.btnHacerPedido.Location = new System.Drawing.Point(897, 605);
+            this.btnHacerPedido.Name = "btnHacerPedido";
+            this.btnHacerPedido.Size = new System.Drawing.Size(130, 23);
+            this.btnHacerPedido.TabIndex = 7;
+            this.btnHacerPedido.Text = "Hacer el pedido";
+            this.btnHacerPedido.UseVisualStyleBackColor = true;
+            this.btnHacerPedido.Click += new System.EventHandler(this.btnHacerPedido_Click);
             // 
             // Pedidos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1466, 690);
+            this.Controls.Add(this.btnHacerPedido);
+            this.Controls.Add(this.btnPedido);
+            this.Controls.Add(this.grdPedidos);
             this.Controls.Add(this.grdMateriaPrima);
             this.Controls.Add(this.lstProductos);
             this.Controls.Add(this.cmbCategorias);
@@ -73,6 +116,7 @@ namespace TFGEscrit
             this.Text = "Pedidos";
             this.Load += new System.EventHandler(this.Pedidos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grdMateriaPrima)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPedidos)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -82,5 +126,8 @@ namespace TFGEscrit
         private System.Windows.Forms.ComboBox cmbCategorias;
         private System.Windows.Forms.ListBox lstProductos;
         private System.Windows.Forms.DataGridView grdMateriaPrima;
+        private System.Windows.Forms.DataGridView grdPedidos;
+        private System.Windows.Forms.Button btnPedido;
+        private System.Windows.Forms.Button btnHacerPedido;
     }
 }
--- TFGEscrit/Pedidos.cs
+++ TFGEscrit/Pedidos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -26,6 +27,14 @@ namespace TFGEscrit
 
         SqlCommand cmdMateriaPrima = new SqlCommand("SELECT M.IdMateriaPrima,Nombre,Precio,Stock,StockProximo FROM Produccion.MateriaPrima M JOIN Produccion.LineaProduccion L ON M.IdMateriaPrima=l.IdMateriaPrima JOIN Produccion.Produccion P ON p.IdProduccion=l.IdProduccion WHERE p.IdProducto=@p_idProducto", conexion);
         SqlParameter p_idProducto = new SqlParameter();
+        DataTable taMateriaPrima = new DataTable();
+
+        DataTable taMateriaPedido = new DataTable();
+
+        SqlCommand cmdHacerPedido = new SqlCommand("Produccion.HacerPedido", conexion);
+        SqlParameter p_idMateriaPrima = new SqlParameter();
+        SqlParameter p_cantidad = new SqlParameter();
+        SqlParameter p_salida = new SqlParameter();
         public Pedidos()
         {
             InitializeComponent();
@@ -33,11 +42,42 @@ namespace TFGEscrit
 
         private void Pedidos_Load(object sender, EventArgs e)
         {
+            /*HACER PEDIDO*/
+            cmdHacerPedido.CommandType = CommandType.StoredProcedure;
+
+            p_idMateriaPrima.ParameterName = "@p_idMateriaPrima";
+            p_idMateriaPrima.SqlDbType = SqlDbType.TinyInt;
+            p_idMateriaPrima.Direction = ParameterDirection.Input;
+
+            p_cantidad.ParameterName = "@p_cantidad";
+            p_cantidad.SqlDbType = SqlDbType.Int;
+            p_cantidad.Direction = ParameterDirection.Input;
+
+            p_salida.ParameterName = "@p_salida";
+            p_salida.SqlDbType = SqlDbType.Int;
+            p_salida.Direction = ParameterDirection.Output;
+
+            cmdHacerPedido.Parameters.Add(p_idMateriaPrima);
+            cmdHacerPedido.Parameters.Add(p_cantidad);
+            cmdHacerPedido.Parameters.Add(p_salida);
+            /*HACER PEDIDO*/
+
             /*MATERIA PRIMA*/
             p_idProducto.ParameterName = "@p_idProducto";
             p_idProducto.SqlDbType = SqlDbType.Int;
-            
+            cmdMateriaPrima.Parameters.Add(p_idProducto);
             /*MATERIA PRIMA*/
+
+            grdPedidos.DataSource = taMateriaPedido;
+            p_idProducto.Value = 0;
+
+            adaptador = new SqlDataAdapter(cmdMateriaPrima);
+            adaptador.FillSchema(taMateriaPedido, SchemaType.Source);
+            adaptador.FillSchema(taMateriaPrima, SchemaType.Source);
+            taMateriaPedido.Columns.Add("Total");
+            grdMateriaPrima.DataSource = taMateriaPrima;
+
+            
 
             /*COMBO BOX DE CATEGORÍAS*/
             adaptador = new SqlDataAdapter(cmdCategorias);
@@ -67,6 +107,7 @@ namespace TFGEscrit
 
         private void cmbCategorias_SelectionChangeCommitted(object sender, EventArgs e)
         {
+            taMateriaPrima.Clear();
             if (lstProductos.Items.Count > 0)
             {
                 lstProductos.Items.Clear();
@@ -83,8 +124,69 @@ namespace TFGEscrit
 
         private void lstProductos_DoubleClick(object sender, EventArgs e)
         {
-            p_idProducto.Value = "";
+            try
+            {
+                taMateriaPrima.Clear();
+                if (lstProductos.SelectedItem != null)
+                {
+                    string codigo = lstProductos.SelectedItem.ToString().Split('-')[0];
+                    p_idProducto.Value = codigo;
+                    adaptador = new SqlDataAdapter(cmdMateriaPrima);
+                    adaptador.Fill(taMateriaPrima);
+                }
+                
+            }
+            catch(Exception ex)
+            {
+
+            }
             
+        }
+
+        private void btnPedido_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+                int cantidad;
+                for (int i = 0; i < grdPedidos.Rows.Count; i++)
+                {
+                    if (grdPedidos.Rows[i].Cells[0].Value.ToString().Equals(grdMateriaPrima.Rows[grdMateriaPrima.SelectedRows[0].Index].Cells[0].Value.ToString()))
+                    {
+                        cantidad = Int32.Parse(Interaction.InputBox("Inserte una cantidad"));
+                        grdPedidos.Rows[i].Cells[4].Value = Int32.Parse(grdPedidos.Rows[i].Cells[4].Value.ToString()) +cantidad;
+                        grdPedidos.Rows[i].Cells[5].Value = float.Parse(grdPedidos.Rows[i].Cells[5].Value.ToString()) + (cantidad * float.Parse(grdMateriaPrima.Rows[grdMateriaPrima.SelectedRows[0].Index].Cells[2].Value.ToString()));
+                        return;
+                    }
+                }
+                //MessageBox.Show(grdPedidos.Rows[0].Cells[0].Value.ToString());
+
+                cantidad = Int32.Parse(Interaction.InputBox("Inserte una cantidad"));
+                DataRow fila = taMateriaPedido.NewRow();
+                fila["IdMateriaPrima"] = grdMateriaPrima.Rows[grdMateriaPrima.SelectedRows[0].Index].Cells[0].Value.ToString();
+                fila["Nombre"] = grdMateriaPrima.Rows[grdMateriaPrima.SelectedRows[0].Index].Cells[1].Value.ToString();
+                fila["Precio"] = grdMateriaPrima.Rows[grdMateriaPrima.SelectedRows[0].Index].Cells[2].Value.ToString();
+                fila["Stock"] = grdMateriaPrima.Rows[grdMateriaPrima.SelectedRows[0].Index].Cells[3].Value.ToString();
+                fila["StockProximo"] = cantidad;
+                fila["Total"] = cantidad * float.Parse(grdMateriaPrima.Rows[grdMateriaPrima.SelectedRows[0].Index].Cells[2].Value.ToString());
+                taMateriaPedido.Rows.Add(fila);
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+        }
+
+        private void btnHacerPedido_Click(object sender, EventArgs e)
+        {
+            
+            conexion.Open();
+            for(int i = 0; i < grdPedidos.Rows.Count; i++)
+            {
+                p_idMateriaPrima.Value = grdPedidos.Rows[i].Cells[0].Value.ToString();
+                p_cantidad.Value = grdPedidos.Rows[i].Cells[4].Value.ToString();
+                cmdHacerPedido.ExecuteNonQuery();
+            }
+            conexion.Close();
         }
     }
 }
